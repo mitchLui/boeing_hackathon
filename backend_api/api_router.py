e2571ce46fceb3a42e645e511ad303eaeedb7601
Mitch Lui <katinlui@gmail.com>
2021-03-20 13:19:18 +0000

    Added boilerplate function

--- backend_api/api_router.py
+++ backend_api/api_router.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-import requests
+from loguru import logger
 
 class APIs:
 
     def __init__(self) -> None:
         pass
+
+    def get_weather(self, location: str) -> dict:
+        return {}
 
     def main(self) -> str:
         return {"ok": "ok"}
