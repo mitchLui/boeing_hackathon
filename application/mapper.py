78aca937ab4b04d01c594d93e7bdf4dbd25f4608
Mitch Lui <katinlui@gmail.com>
2021-03-20 13:33:17 +0000

    Added type hinting return

--- application/mapper.py
+++ application/mapper.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from loguru import logger
+from typing import Tuple
 import gmplot
 
 class Mapper:
@@ -7,8 +8,10 @@ class Mapper:
     def __init__(self) -> None:
         pass
 
-    def main(self) -> str:
-        return "ok"
+    def main(self, rtype: str) -> Tuple(str | dict, int):
+        if rtype == "json": return {"ok", "ok"}, 200
+        elif rtype == "html": return "ok", 200
+        else: return "Unsupported", 400
 
 if __name__ == "__main":
     mapper = Mapper()
